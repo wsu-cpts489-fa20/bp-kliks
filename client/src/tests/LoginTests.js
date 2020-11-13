d9d61a99ccf457a30d1c91807ef8a08a4028004a
marcoarceo <marcoarceo@outlook.com>
2020-11-12 22:09:53 -0800

    Adding tests to make sure the login page is working properly. Need to review previous notes to make sure I remember how to.

--- client/src/tests/LoginTests.js
+++ client/src/tests/LoginTests.js
@@ -1,12 +1,26 @@
+import { async } from 'regenerator-runtime'
 import { Selector } from 'testcafe';
 
 // fixture `SpeedScore Interactions`
 //     .page `http://127.0.0.1:5500/index.html`;
 
 fixture `Kliks Login Tests`
-    .page `https://kliks.bfapp.org/`; //http://localhost/    
-
+    .page `https://kliks.bfapp.org/`; //http://localhost/  
+    
 test('LoginGoesToMainPage', async t => {
+    await t
+    .typeText('#emailInput', 'test35@wsu.edu')
+    .typeText('#passwordInput', 'ASDFasdf12345')
+    .click('#loginBtn')
+    .expect(Selector('#feedModeDiv').visible).eql(true)
+});
+
+test('LoginGoesToCreate', async t => {
+    await t
+        .expect(true).ok("This is a test to check to make sure GitHub Actions TestCafe workflow works");
+});
+
+test('LoginGoesToReset', async t => {
     await t
         .expect(true).ok("This is a test to check to make sure GitHub Actions TestCafe workflow works");
 });
