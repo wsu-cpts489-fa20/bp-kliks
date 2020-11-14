dab308433668270e808138cf5e8566167725f14f
Osman Bakari <osman.bakari@wsu.edu>
2020-11-13 16:37:52 -0800

    fixing an issue with deploying

---
+++ client/build/precache-manifest.951b49fafac2df66deea8b7837339cc2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e21f0d14f89fb6052b8c01e0b4d6fe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ff9c0d9ea1368ad8f9b",
+    "url": "/static/css/2.00b6ad22.chunk.css"
+  },
+  {
+    "revision": "4e82fb59d6aacf69afd2",
+    "url": "/static/css/main.bf2a951b.chunk.css"
+  },
+  {
+    "revision": "1ff9c0d9ea1368ad8f9b",
+    "url": "/static/js/2.bc8c62b2.chunk.js"
+  },
+  {
+    "revision": "3c6f56c7898dadf6e53f57cb4b6406e3",
+    "url": "/static/js/2.bc8c62b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e82fb59d6aacf69afd2",
+    "url": "/static/js/main.b0338156.chunk.js"
+  },
+  {
+    "revision": "19793d9fa96413d1c44d",
+    "url": "/static/js/runtime-main.0d575dc6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
