3edd8f027f318d1456e59673e0bae2433309abfe
Osman Bakari <osman.bakari@wsu.edu>
2020-11-15 13:41:34 -0800

    Added the test files for the app.

---
+++ client/src/tests/SurveyManagementTests.js
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe';
+
+var isLocalTesting = true;
+const DEPLOY_TEST_URL = 'https://kliks.bfapp.org/';
+const DEV_TEST_URL = 'http://localhost:8081';
+
+
+fixture `Questions Tests`
+    .page(isLocalTesting ? DEV_TEST_URL : DEV_TEST_URL);
+
+test('Test Tempalte', async t => {
+    await t
+    .expect(true).ok("This is a test to check to make sure GitHub Actions TestCafe workflow works");
+});
