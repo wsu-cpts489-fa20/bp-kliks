96c5235ef95ccf09d07fbed1d08365e7b0402980
rebecca <rebecca.rothschild@wsu.edu>
2020-11-19 00:06:25 -0800

    RR - implemented the basic courses and students table

--- client/src/components/CourseManagement/CoursesPage.js
+++ client/src/components/CourseManagement/CoursesPage.js
@@ -1,46 +1,66 @@
 import React from 'react';
 import SearchField from 'react-search-field'
+import AppMode from '../../AppMode';
+import StudentsTable from './StudentsTable.js';
+import CoursesTable from './CoursesTable';
 
 class CoursesPage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            currentCourse = "",
+            mode: AppMode.COURSES,
+            courseId: "",
+            courseName: "",
+            students: [],
         };
+    }
+
+    handleChangeMode = (newMode) => {
+        this.setState({mode: newMode});
+
+        // if state changed to courses, possibly reset the course id/name
+    }
+
+    handleChangeCourse = async (courseId, courseName) => {
+        this.setState({
+            courseId: courseId,
+            courseName: courseName,
+        });
+
+        // Get the courses students
+        let url = "/students/" + courseId;
+        let res = await fetch(url, {method: 'GET'});
+        if (res.status != 200) {
+            let msg = await res.text();
+            console.log("There was an error obtaining students in this course: " + msg);
+            return;
+        } 
+        let body = await res.json();
+        body = JSON.parse(body);
+        console.log("in componentDidMount with GET results: " + body);
+        this.setState({students: body});
     }
 
     render() {
         return (
             <div className="padded-page">
                 <center>
-                <h1></h1>
+                <h1>{this.state.mode === AppMode.COURSES ? "Courses" : "Students in " + this.state.courseName}</h1>
+
+                {this.state.mode === AppMode.COURSES ?
+                    <CoursesTable 
+                    courses={this.props.userObj.courses}
+                    menuOpen={this.props.menuOpen}
+                    changeMode={this.handleChangeMode}
+                    changeCourse={this.handleChangeCourse}/> :
+                    <StudentsTable 
+                    students={this.state.students}
+                    menuOpen={this.props.menuOpen}
+                    changeMode={this.handleChangeMode}/>
+                }
                 
-                <div className="input-group center-search" style={{justifyContent: "center", paddingBottom: "20px"}}>
-                    <SearchField
-                        classNames="search-width"
-                        placeholder="Search question"
-                        onSearchClick={this.onSearchClicked}
-                    />
-                    {/* <span className="input-group-prepend">
-                        <div className="input-group-text bg-transparent border-right-0"><i className="fa fa-search"></i></div>
-                    </span>
-                    <input className="form-control py-2 border-left-0 border" placeholder="Search Rounds" type="search" value="" id="searchRounds"
-                    onkeyup="searchRoundsTable(this.value)" onSearch onsearch="searchRoundsTable(this.value)"/> */}
-                </div>
-                <table className="table table-hover">
-                <thead className="thead-light">
-                <tr>
-                    <th>Course Name</th>
-                    <th>Year</th>
-                    <th>Semester</th>
-                    <th>Instructor Name</th>
-                    <th>View Students</th>
-                </tr>
-                </thead>
-                <tbody>
-                </tbody>
-                </table>
+                
                 </center>
             </div>
         );
