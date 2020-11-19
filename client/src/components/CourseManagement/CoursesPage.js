053bea0b4fee09fcc8050a69637977bf07575208
rebecca <rebecca.rothschild@wsu.edu>
2020-11-18 21:52:11 -0800

    RR - added base component files for managing courses

--- client/src/components/CourseManagement/CoursesPage.js
+++ client/src/components/CourseManagement/CoursesPage.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import SearchField from 'react-search-field'
 
 class CoursesPage extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentCourse = "",
+        };
+    }
+
     render() {
         return (
-        <div className="padded-page">
-            <center>
-            <h1 >Courses</h1>
-            <h2>This page is under construction.</h2>
-            <img src="https://dl.dropboxusercontent.com/s/qpjhy9x9gwdxpob/SpeedScoreLogo64Trans.png" 
-             height="200" width="200"/>
-            <p style={{fontStyle: "italic"}}>Version CptS 489 React Demo</p>
-            </center>
-        </div>
+            <div className="padded-page">
+                <center>
+                <h1></h1>
+                
+                <div className="input-group center-search" style={{justifyContent: "center", paddingBottom: "20px"}}>
+                    <SearchField
+                        classNames="search-width"
+                        placeholder="Search question"
+                        onSearchClick={this.onSearchClicked}
+                    />
+                    {/* <span className="input-group-prepend">
+                        <div className="input-group-text bg-transparent border-right-0"><i className="fa fa-search"></i></div>
+                    </span>
+                    <input className="form-control py-2 border-left-0 border" placeholder="Search Rounds" type="search" value="" id="searchRounds"
+                    onkeyup="searchRoundsTable(this.value)" onSearch onsearch="searchRoundsTable(this.value)"/> */}
+                </div>
+                <table className="table table-hover">
+                <thead className="thead-light">
+                <tr>
+                    <th>Course Name</th>
+                    <th>Year</th>
+                    <th>Semester</th>
+                    <th>Instructor Name</th>
+                    <th>View Students</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                </table>
+                </center>
+            </div>
         );
     }   
 }
---
+++ client/src/components/CourseManagement/StudentsPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class StudentsPage extends React.Component {
+
+    render() {
+        return (
+        <div className="padded-page">
+            <center>
+            <h1 >Students</h1>
+            <h2>This page is under construction.</h2>
+            <img src="https://dl.dropboxusercontent.com/s/qpjhy9x9gwdxpob/SpeedScoreLogo64Trans.png" 
+             height="200" width="200"/>
+            </center>
+        </div>
+        );
+    }   
+}
+
+export default StudentsPage;
