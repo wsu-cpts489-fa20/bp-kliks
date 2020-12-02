3d27176cc5eef9bec8b0decbb9e9d65739e2c3ad
Osman <odbakari@gmail.com>
2020-12-02 03:02:26 -0800

    -ob-forgot to stage the CreateSurvey file.

--- client/src/components/SurveyManagement/CreateSurvey.js
+++ client/src/components/SurveyManagement/CreateSurvey.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FileUpload from './AnswerTypes/FileUpload';
 import FreeResponse from './AnswerTypes/FreeResponse';
 import MultipleChoice from './AnswerTypes/MultipleChoice';
+import CreateQuestion from './CreateQuestion';
 
 
 const answerTypes = {};
@@ -12,11 +13,18 @@ answerTypes["fileUpload"] = FileUpload;
 class CreateSurvey extends React.Component {
 
     //Initialize a Rounds object based on local storage
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        //Create date object for today, taking time zone into consideration
+        let today = new Date(Date.now()-(new Date()).getTimezoneOffset()*60000);        
         this.state = {
             answerType : "shortAnswer",
-            question: ""
+            question: "",
+            addSurvey: false,
+            surveyTitle : "",
+            date: today.toISOString().substr(0,10),
+            dropdownOfCourses : ""
         }     
     }
 
@@ -27,43 +35,58 @@ class CreateSurvey extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Create Question");
+
+        console.log("Create Survey");
+    }
+
+
+    getCourses = () => {
+      var courses = [];
+
+      for(var index = 0; index < this.props.userObj.courses.length; index++)
+      {
+        const id = this.props.userObj.courses[index].courseSemester + "-"  + this.props.userObj.courses[index].courseYear +
+        ": " + this.props.userObj.courses[index].courseName + "  " + this.props.userObj.courses[index].courseNumber;
+        courses.push(<option id={id} value={id}>{id}</option>);
+      }
+
+      // let courses = this.props.userObj.courses.map((course) => {
+      //   return {
+      //     courseID : course.courseID,
+
+      //   };
+      // });
+     return courses;
     }
 
     render(){
-        const AnswerType = answerTypes[this.state.answerType];
         return(
             <form className="padded-page" onSubmit={this.handleSubmit}>
             <center>
               <label>
-                Question Title:
-                <input name="title" className="form-control form-center" 
+                Survey Title:
+                <input name="surveyTitle" className="form-control form-center"
+                value={this.state.surveyTitle}
                   type="text"/>
               </label>
               <p></p>
-                <label>Question:
-                    <textarea name="question" className="form-control" rows="6" cols="75" 
-                    placeholder="Enter Question here" value={this.state.question} 
-                    />
-                </label>
+              <label>
+                Date:
+                <input name="date" className="form-control form-center" 
+                  type="date" value={this.state.date} onChange={this.handleChange} />
+              </label>
+              <p></p>
             <p></p>
-            <p></p>
-            <label>Answer Type:
-            <select name="answerType" value={this.state.answerType} onChange={this.handleChange} 
+            <label>Course:
+            <select name="dropdownOfCourses" value={this.state.dropdownOfCourses} onChange={this.handleChange} 
               className="form-control form-center">
-              <option value="shortAnswer">Short Answer</option>
-              <option value="multipleChoice">Multiple Choice</option>
-              <option value="fileUpload">File Upload</option>
+                {this.getCourses()}
             </select> 
             </label>
             <p></p>
-            <AnswerType>
-
-            </AnswerType>
-            <p></p>
-            <button type="submit" style={{width: "70%",fontSize: "36px"}} 
-              className="btn btn-primary btn-color-theme">
-                <span className="fa fa-plus"/>&nbsp; Create Question
+            <button type="submit" style={{width: "50%",fontSize: "36px"}}
+            className="btn btn-primary btn-color-theme">
+              <span className="fa fa-plus"/>&nbsp; Create survey
             </button>
             </center>
           </form>            
