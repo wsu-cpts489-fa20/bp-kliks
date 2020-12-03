f17c190f0a0cd165af55a912a8e776e88544ae45
Derosa <derosa@eecs.wsu.edu>
2020-12-03 14:24:39 -0800

    Added button to search questions page and populated the table, as well as added an isactive question tab underneath the table just for testing purposes, can remove later.

--- client/src/components/SurveyManagement/SearchQuestions.js
+++ client/src/components/SurveyManagement/SearchQuestions.js
@@ -2,23 +2,80 @@ import React from 'react';
 import SearchField from 'react-search-field'
 //import ConfirmDeleteRound from './ConfirmDeleteRound.js';
 // import AppMode from './../AppMode.js';
+import Switch from "react-switch";
 
 class SubmittedResponse extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { checked: false ,
+          showConfirmDelete: false};
+    this.handleChange = this.handleChange.bind(this);
   }
+
+
+    //called when the toggle button is clicked
+    handleChange(checked) {
+      this.setState({ checked });
+    }
 
     onSearchClicked = () => {
         console.log("onSearchClicked");
     }
+
+    editRound = (id) => {
+      //this.props.setEditId(id);
+      //this.props.changeMode(AppMode.SURVEY_MANAGEMENT);
+    }
+
+    deleteRound = () => {
+      //this.props.deleteRound();
+      //this.setState({showConfirmDelete:false});
+  
+    }
+
+    confirmDelete = (id) => {
+      //this.props.setDeleteId(id);
+      //this.setState({showConfirmDelete:true});
+    }
+
+    hideConfirmDeleteBox = () => {
+      //this.setState({showConfirmDelete:false});
+    }
+
+    renderTable = () => {
+      let table = [];
+      for (const r in this.props.question) {
+        table.push(
+          <tr key={r}>
+            <td>{this.props.question[r].questionTitle}</td>
+            <td>{this.props.question[r].questionText}</td>
+            <td>{this.props.question[r].questionAnswers}</td>
+            <td>{this.props.question[r].questionActive}</td>
+            <td><button onClick={this.props.menuOpen ? null : null}>
+                  <span className="fa fa-eye"></span></button></td>
+            <td><button onClick={this.props.menuOpen ? null : 
+              null}>
+                  <span className="fa fa-trash"></span></button></td>
+          </tr> 
+        );
+      }
+      return table;
+      }
   //render--render the entire rounds table with header, displaying a "No
   //Rounds Logged" message in case the table is empty.
   render() {
     return(
     <div className="padded-page">
       <center>
-      <h1></h1>
-        
+      <h1>Questions Baby!</h1>
+      <label htmlFor="normal-switch">
+        <span><h1>Active Question</h1></span>
+        <Switch
+          onChange={this.handleChange}
+          checked={this.state.checked}
+          id="normal-switch"
+        />
+      </label>
          <div className="input-group center-search" style={{justifyContent: "center", paddingBottom: "20px"}}>
             <SearchField
               classNames="search-width"
@@ -34,13 +91,16 @@ class SubmittedResponse extends React.Component {
       <table className="table table-hover">
         <thead className="thead-light">
         <tr>
-          <th>Question Title</th>
+          <th>Title</th>
           <th>Question</th>
           <th>Answer Type</th>
+          <th>Is Active</th>
           <th>View/Edit...</th>
           <th>Delete</th>
         </tr>
         </thead>
+        <tbody>
+        </tbody>
         <tbody>
         </tbody>
       </table>
