811160c8f0f27f4a7565bfa6e7a5c765a089d9c7
marcoarceo <marcoarceo@outlook.com>
2020-12-03 14:45:41 -0800

    marco arceo - added the ability to populate the table depending on the switch mode that is selected by the user. This progress is associated with issue #41

--- client/src/components/SurveyManagement/SearchQuestions.js
+++ client/src/components/SurveyManagement/SearchQuestions.js
@@ -6,12 +6,84 @@ import SearchField from 'react-search-field'
 class SubmittedResponse extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {ActiveFilter: false};
+    this.state = {ActiveFilter: false,
+                  checked: false ,
+                  showConfirmDelete: false};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  //called when the toggle button is clicked
+  handleChange(checked) {
+    this.setState({ checked });
   }
 
   onSearchClicked = () => {
       console.log("onSearchClicked");
   }
+
+  editRound = (id) => {
+    //this.props.setEditId(id);
+    //this.props.changeMode(AppMode.SURVEY_MANAGEMENT);
+  }
+
+  deleteRound = () => {
+    //this.props.deleteRound();
+    //this.setState({showConfirmDelete:false});
+
+  }
+
+  confirmDelete = (id) => {
+    //this.props.setDeleteId(id);
+    //this.setState({showConfirmDelete:true});
+  }
+
+  hideConfirmDeleteBox = () => {
+    //this.setState({showConfirmDelete:false});
+  }
+
+  renderTable = () => {
+    let table = [];
+    if (this.state.ActiveFilter == false)
+    {
+      for (const r in this.props.question) {
+        table.push(
+          <tr key={r}>
+            <td>{this.props.question[r].questionTitle}</td>
+            <td>{this.props.question[r].questionText}</td>
+            <td>{this.props.question[r].questionAnswers}</td>
+            <td>{this.props.question[r].questionActive}</td>
+            <td><button onClick={this.props.menuOpen ? null : null}>
+                  <span className="fa fa-eye"></span></button></td>
+            <td><button onClick={this.props.menuOpen ? null : 
+              null}>
+                  <span className="fa fa-trash"></span></button></td>
+          </tr> 
+        );
+      }
+    }
+    else if (this.state.ActiveFilter == true)
+    {
+      for (const r in this.props.question) {
+        if (this.props.question[r].questionActive == true)
+        {
+          table.push(
+            <tr key={r}>
+              <td>{this.props.question[r].questionTitle}</td>
+              <td>{this.props.question[r].questionText}</td>
+              <td>{this.props.question[r].questionAnswers}</td>
+              <td>{this.props.question[r].questionActive}</td>
+              <td><button onClick={this.props.menuOpen ? null : null}>
+                    <span className="fa fa-eye"></span></button></td>
+              <td><button onClick={this.props.menuOpen ? null : 
+                null}>
+                    <span className="fa fa-trash"></span></button></td>
+            </tr> 
+          );
+        }
+      }
+    }
+    return table;
+    }
 
   switchHandler = () => {
     console.log(this.state.ActiveFilter);
@@ -24,7 +96,7 @@ class SubmittedResponse extends React.Component {
       this.setState({ActiveFilter: false});
     }
   }
-  
+
   //render--render the entire rounds table with header, displaying a "No
   //Rounds Logged" message in case the table is empty.
   render() {
@@ -37,7 +109,6 @@ class SubmittedResponse extends React.Component {
         <span class="on">Active</span><span class="off">All</span>
         </div>
       </label>
-
          <div className="input-group center-search" style={{justifyContent: "center", paddingBottom: "20px"}}>
             <SearchField
               classNames="search-width"
@@ -53,13 +124,23 @@ class SubmittedResponse extends React.Component {
       <table className="table table-hover">
         <thead className="thead-light">
         <tr>
-          <th>Question Title</th>
+          <th>Title</th>
           <th>Question</th>
           <th>Answer Type</th>
+          <th>Is Active</th>
           <th>View/Edit...</th>
           <th>Delete</th>
         </tr>
         </thead>
+        <tbody>
+          {/* {Object.keys(this.props.question).length === 0 ? 
+            <tr>
+            <td colSpan="6" style={{fontStyle: "italic"}}>No questions to the table</td>
+            </tr> : this.renderTable()
+          } */}
+          </tbody>
+        <tbody>
+        </tbody>
         <tbody>
         </tbody>
       </table>
